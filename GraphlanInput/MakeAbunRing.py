234b823e5538c8735045cf86442d4d242cdb40c3
Jin Choi <jinchoi721@gmail.com>
2015-10-05 12:16:42 -0500

    update

--- GraphlanInput/MakeAbunRing.py
+++ GraphlanInput/MakeAbunRing.py
@@ -4,7 +4,7 @@
 # python MakeAbunRing.py SoilAbundance.txt SoilAbunAnno.txt
 
 import sys
-
+import math
 fread = open(sys.argv[1],'r')
 fwrite = open(sys.argv[2],'w')
 
@@ -19,5 +19,6 @@ for line in fread:
     tempcol = line.strip().split('\t')
     tempNum = labelNumStart
     for i in range(2,len(tempcol)):
-        fwrite.write(tempcol[0]+'\t'+"ring_alpha"+'\t'+str(tempNum)+'\t'+tempcol[i]+'\n')
+        tempAlpha = math.log(float(tempcol[i]),10)
+        fwrite.write(tempcol[0]+'\t'+"ring_alpha"+'\t'+str(tempNum)+'\t'+str(tempAlpha)+'\n')
         tempNum = tempNum + 1
