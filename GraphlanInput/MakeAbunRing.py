e7ab0e655db42f8bdb6f8a529d0809aaf6be3e8a
Jin Choi <jinchoi721@gmail.com>
2015-10-05 12:42:45 -0500

    update

--- GraphlanInput/MakeAbunRing.py
+++ GraphlanInput/MakeAbunRing.py
@@ -15,10 +15,26 @@ for i in range(2,len(header)):
     fwrite.write("ring_label"+'\t'+str(tempNum)+'\t'+header[i]+'\n')
     tempNum = tempNum+1
 tempNum = labelNumStart
+
+table = []
 for line in fread:
     tempcol = line.strip().split('\t')
+    tempTable = []
+    for i in range(len(tempcol)):
+        tempTable.append(tempcol[i])
+    table.append(tempTable)
+lar = []
+for i in range(len(header)):
+    lar.append(0.0)
+
+for i in range(len(table)):
+    for j in range(2,len(table[i])):
+        if (lar[j] < float(table[i][j])):
+            lar[j]=float(table[i][j])
+
+for i in range(len(table)):
     tempNum = labelNumStart
-    for i in range(2,len(tempcol)):
-        tempAlpha = math.log(float(tempcol[i]),2)
-        fwrite.write(tempcol[0]+'\t'+"ring_alpha"+'\t'+str(tempNum)+'\t'+str(tempAlpha)+'\n')
+    for j in range(2,len(table[i])):
+        tempAlpha = math.log(float(table[i][j]),10)
+        fwrite.write(table[i][0]+'\t'+"ring_alpha"+'\t'+str(tempNum)+'\t'+str(tempAlpha)+'\n')
         tempNum = tempNum + 1
