c67b04af013ac8b414ebc68344db8f3a021a7495
Jin Choi <jinchoi721@gmail.com>
2015-10-12 11:14:59 -0500

    update

---
+++ GraphlanInput/MergeAbunTax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# example: python MergeAbunTax.py Refsoil16scomp_tax_assignments.txt abunTable.txt abun_w_tax.txt
+import sys
+fread1 = open(sys.argv[1],'r')
+fread2 = open(sys.argv[2],'r')
+fwrite = open(sys.argv[3],'w')
+
+tax = []
+for line in fread1:
+    tempcol = line.strip().split('\t')
+    temptax = [tempcol[0],line.strip()]
+    tax.append(temptax)
+
+for line in fread2:
+    tempcol = line.strip().split('\t')
+    for i in range(len(tax)):
+        if(tempcol[0]==tax[i][0]):
+            fwrite.write(line.strip()+'\t'+tax[i][1]+'\n')
