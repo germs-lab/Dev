bca76840968e1425f0b990a905ab929547d3a9ad
Jin Choi <jinchoi721@gmail.com>
2015-10-12 15:49:13 -0500

    update

--- GraphlanInput/MakeAnno.py
+++ GraphlanInput/MakeAnno.py
@@ -1,6 +1,7 @@
 #python
-#python MakeAnno.py rep_set_tax_assignments.txt Default.txt anno.txt
+#python MakeAnno.py rep_set_tax_assignments.txt DefaultAnno.txt anno.txt
 import sys
+import modules
 filein = sys.argv[1]
 filedefault =  sys.argv[2]
 fileout = sys.argv[3]
@@ -15,76 +16,24 @@ for line in deread:
 fwrite.write('\n')
 
 #Make Tax table
-Kingdom = []
-phylum = []
-Class = []
-Order = []
-Family = []
-genus = []
-Sp = []
-Tax = []
-ID = []
-for line in fread:
-    tempLine = line.split(';')
-    tempTax = []
-    tempKingdom = ""
-    tempPhylum = ""
-    tempClass = ""
-    tempOrder = ""
-    tempFamily = ""
-    tempGenus = ""
-    tempSp = ""
-    tempID = ""
-    for i in range(len(tempLine)):
-        tempcol = tempLine[i].split('__')
-        if (tempcol[0][-1:]=="k"):
-            incase = tempcol[1].split('\t')
-            fortempID = tempcol[0].split('\t')
-            if(len(incase)==1):
-                Kingdom.append(tempcol[1])
-                tempKingdom = tempcol[1]
-                tempID = fortempID[0]
-        elif (tempcol[0]==" p"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                phylum.append(tempcol[1])
-                tempPhylum = tempcol[1]
-        elif (tempcol[0]==" c"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Class.append(tempcol[1])
-                tempClass = tempcol[1]
-        elif (tempcol[0]==" o"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Order.append(tempcol[1])
-                tempOrder = tempcol[1]
-        elif (tempcol[0]==" f"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Family.append(tempcol[1])
-                tempFamily = tempcol[1]
-        elif (tempcol[0]==" g"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                genus.append(tempcol[1])
-                tempGenus = tempcol[1]
-        elif (tempcol[0]==" s"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==3):
-                
-                Sp.append(incase[0])
-                tempSp = incase[0]
-    tempTax = [tempKingdom,tempPhylum,tempClass,tempOrder,tempFamily,tempGenus,tempSp,tempID]
-    Tax.append(tempTax)
+Tax = modules.TaxTable(filein)
 
 #print Tax
+phylum = []
+Family = []
+Class = []
+genus = []
+for i in range(len(Tax)):
+	phylum.append(Tax[i][1])
+	Family.append(Tax[i][4])
+	Class.append(Tax[i][2])
+	genus.append(Tax[i][5])
 uniqPhylum = list(set(phylum))
 uniqFamily = list(set(Family))
 uniqClass = list(set(Class))
 uniqGenus = list(set(genus))
 
-color = ['r','g','b','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#800080','#191970','#7B68EE','#00CD00','#8B4513','#BC8F8F','#303030','#8E8E38','#CDCDC1','r','g','b','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#8000\
+color = ['r','g','b','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#800080','#191970','#7B68EE','#00CD00','#8B4513','#BC8F8F','#303030','#8E8E38','#CDCDC1','#D15FEE','#FFC0CB','#800000','#808080','#B0171F','r','g','b','#FFC0CB','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#8000\
 80','#191970','#7B68EE','#00CD00','#8B4513','#BC8F8F','#303030','#8E8E38','#CDCDC1','r','g','b','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#8000\
 80','#191970','#7B68EE','#00CD00','#8B4513','#BC8F8F','#303030','#8E8E38','#CDCDC1','r','g','b','#EE6A50','#9ACD32','#87CEFA','#FFC125','#8DEEEE','#006400','#8000\
 80','#191970','#7B68EE','#00CD00','#8B4513','#BC8F8F','#303030','#8E8E38','#CDCDC1']
@@ -113,34 +62,8 @@ for i in range(len(Tax)):
         fwrite.write(Tax[i][5]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
         fwrite.write(Tax[i][5]+'\t'+"ring_width"+'\t'+"1"+'\t'+"2"+'\n')
         fwrite.write(Tax[i][5]+'\t'+"ring_height"+'\t'+"1"+'\t'+"0.35"+'\n')
-        #fwrite.write(Tax[i][5]+'\t'+"ring_shape"+'\t'+"1"+'\t'+"v"+'\n')
         fwrite.write(Tax[i][5]+'\t'+"ring_color"+'\t'+"1"+'\t'+kingColor+'\n')
         if(Tax[i][6] != ""):
             fwrite.write(Tax[i][6]+'\t'+"clade_marker_color"+'\t'+tempColor+'\n')
             fwrite.write(Tax[i][6]+'\t'+"clade_marker_size"+'\t'+"30"+'\n')
             fwrite.write(Tax[i][6]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
-            #fwrite.write(Tax[i][5]+'\t'+"ring_width"+'\t'+"2"+'\t'+"4"+'\n')
-            #fwrite.write(Tax[i][5]+'\t'+"ring_height"+'\t'+"2"+'\t'+"0.35"+'\n')
-            #fwrite.write(Tax[i][5]+'\t'+"ring_color"+'\t'+"2"+'\t'+kingColor+'\n')
-
-#for i in range(len(uniqPhylum)):
-#    fwrite.write(uniqPhylum[i]+'\t'+"annotation"+'\t'+uniqPhylum[i]+'\n')
-#    fwrite.write(uniqPhylum[i]+'\t'+"annotation_background_color"+'\t'+color[i]+'\n')
-#    fwrite.write(uniqPhylum[i]+'\t'+"clade_marker_color"+'\t'+"k"+'\n')
-#    fwrite.write(uniqPhylum[i]+'\t'+"clade_marker_size"+'\t'+"3"+'\n')
-
-#for i in range(len(uniqFamily)):
-#    fwrite.write(uniqFamily[i]+'\t'+"clade_marker_color"+'\t'+"b"+'\n')
-#    fwrite.write(uniqFamily[i]+'\t'+"clade_marker_size"+'\t'+"30"+'\n')
-
-#for i in range(len(uniqClass)):
-#    fwrite.write(uniqClass[i]+'\t'+"annotation"+'\t'+"*:"+uniqClass[i]+'\n')
-#    fwrite.write(uniqClass[i]+'\t'+"annotation_background_color"+'\t'+"w"+'\n')
-#    fwrite.write(uniqClass[i]+'\t'+"clade_marker_color"+'\t'+"k"+'\n')
-#    fwrite.write(uniqClass[i]+'\t'+"clade_marker_size"+'\t'+"3"+'\n')
-#    fwrite.write(uniqClass[i]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
-
-#for i in range(len(uniqGenus)):
-#    fwrite.write(uniqGenus[i]+'\t'+"clade_marker_color"+'\t'+color[0]+'\n')
-#    fwrite.write(uniqGenus[i]+'\t'+"clade_marker_size"+'\t'+"30"+'\n')
-#    fwrite.write(uniqGenus[i]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
--- GraphlanInput/MakeAnnoXML.py
+++ GraphlanInput/MakeAnnoXML.py
@@ -1,6 +1,7 @@
 #python
 #python MakeAnno.py rep_set_tax_assignments.txt DefaultAnno.txt Annotation.txt
 import sys
+import modules
 filein = sys.argv[1]
 fileout = sys.argv[3]
 filedefault = sys.argv[2]
@@ -14,68 +15,7 @@ for line in deread:
 fwrite.write('\n')
 
 #Make Tax table
-Kingdom = []
-phylum = []
-Class = []
-Order = []
-Family = []
-genus = []
-Sp = []
-Tax = []
-ID = []
-for line in fread:
-    tempLine = line.split(';')
-    tempTax = []
-    tempKingdom = ""
-    tempPhylum = ""
-    tempClass = ""
-    tempOrder = ""
-    tempFamily = ""
-    tempGenus = ""
-    tempSp = ""
-    tempID = ""
-    for i in range(len(tempLine)):
-        tempcol = tempLine[i].split('__')
-        if (tempcol[0][-1:]=="k"):
-            incase = tempcol[1].split('\t')
-            fortempID = tempcol[0].split('\t')
-            if(len(incase)==1):
-                Kingdom.append(tempcol[1])
-                tempKingdom = tempcol[1]
-                tempID = fortempID[0]
-        elif (tempcol[0]==" p"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                phylum.append(tempcol[1])
-                tempPhylum = tempcol[1]
-        elif (tempcol[0]==" c"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Class.append(tempcol[1])
-                tempClass = tempcol[1]
-        elif (tempcol[0]==" o"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Order.append(tempcol[1])
-                tempOrder = tempcol[1]
-        elif (tempcol[0]==" f"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                Family.append(tempcol[1])
-                tempFamily = tempcol[1]
-        elif (tempcol[0]==" g"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==1):
-                genus.append(tempcol[1])
-                tempGenus = tempcol[1]
-        elif (tempcol[0]==" s"):
-            incase = tempcol[1].split('\t')
-            if(len(incase)==3):
-                
-                Sp.append(incase[0])
-                tempSp = incase[0]
-    tempTax = [tempKingdom,tempPhylum,tempClass,tempOrder,tempFamily,tempGenus,tempSp,tempID]
-    Tax.append(tempTax)
+Tax = modules.TaxTable(filein)
 
 #print Tax
 uniqPhylum = list(set(phylum))
