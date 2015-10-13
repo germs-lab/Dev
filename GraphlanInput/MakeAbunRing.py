c4e20fefce0a758b83e8fb062a94c525843650bb
Jin Choi <jinchoi721@gmail.com>
2015-10-13 14:59:55 -0500

    update

--- GraphlanInput/MakeAbunRing.py
+++ GraphlanInput/MakeAbunRing.py
@@ -8,12 +8,15 @@ import math
 fread = open(sys.argv[1],'r')
 fwrite = open(sys.argv[2],'w')
 
+ringcolor = ['k','r','g','b','#0000FF','#696969','#FF0000','#FFA500','#0000FF','#696969','#FF0000','#FFA500']
+
 # read abundance file
 header = fread.readline().strip().split('\t')
 labelNumStart = 2
 tempNum = labelNumStart
 for i in range(2,len(header)):
     fwrite.write("ring_label"+'\t'+str(tempNum)+'\t'+header[i]+'\n')
+    fwrite.write("ring_color"+'\t'+str(tempNum)+'\t'+ringcolor[tempNum]+'\n')
     tempNum = tempNum+1
 tempNum = labelNumStart
 
@@ -34,12 +37,12 @@ for i in range(len(table)):
     for j in range(2,len(table[i])):
         if (lar[j] < float(table[i][j])):
             lar[j]=float(table[i][j])
-ringcolor = ['k','r','g','b','#0000FF','#696969','#FF0000','#FFA500','#0000FF','#696969','#FF0000','#FFA500']
+
 # Write annotation file
 for i in range(len(table)):
     tempNum = labelNumStart
     for j in range(2,len(table[i])):
         tempAlpha = math.log(float(table[i][j]),10)/math.log(lar[j])
         fwrite.write(table[i][0]+'\t'+"ring_alpha"+'\t'+str(tempNum)+'\t'+str(tempAlpha)+'\n')
-        fwrite.write(table[i][0]+'\t'+"ring_color"+'\t'+str(tempNum)+'\t'+ringcolor[tempNum]+'\n')
+        #fwrite.write(table[i][0]+'\t'+"ring_color"+'\t'+str(tempNum)+'\t'+ringcolor[tempNum]+'\n')
         tempNum = tempNum + 1
