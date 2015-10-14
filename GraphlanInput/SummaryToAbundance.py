29cabf6bf5e44017c4c8389c9d0ca53b7a7a2e09
Jin Choi <jinchoi721@gmail.com>
2015-10-14 15:08:05 -0500

    update

--- GraphlanInput/SummaryToAbundance.py
+++ GraphlanInput/SummaryToAbundance.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 #
-# python SummaryToAbundance.py ../EMP/soil_loam.biom.summary soil.abundance.txt
+# python SummaryToAbundance.py ../EMP/soil_loam.biom.summary RefSoil16sHMMFastaNS_tax_assignments.txt soil.abundance.txt
 
 import sys
+import modules
 inputRead = open(sys.argv[1],'r')
-fwrite = open(sys.argv[2],'w')
+taxRead = sys.argv[2]
+fwrite = open(sys.argv[3],'w')
 
+#read count
+count = []
 flag = 0
 for line in inputRead:
     if (flag == 0):
@@ -13,4 +17,17 @@ for line in inputRead:
             flag = 1
     elif (flag == 1):
         tempcol = line.strip().split(': ')
-        fwrite.write(tempcol[0]+'\t'+tempcol[1]+'\n')
+        #fwrite.write(tempcol[0]+'\t'+tempcol[1]+'\n')
+        tempCount = [tempcol[0],tempcol[1]]
+        count.append(tempCount)
+
+# read Tax
+Tax = modules.TaxTable(taxRead)
+# taxonomy table to guide table
+Guide = modules.TaxToGuide(Tax)
+
+#write result
+for i in range(len(count)):
+    for j in range(len(Guide)):
+        if(count[i][0] == Guide[j][0]):
+            fwrite.write(Guide[j][1]+'\t'+Guide[j][0]+'\t'+count[i][1]+'\n')
