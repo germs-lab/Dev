87d9aed4ebadec930d5ae972371160add1ada741
Jin Choi <jinchoi721@gmail.com>
2015-11-11 17:36:33 -0600

    update

--- GraphlanInput/Guide_Add_Ring_Abundance_EMP.py
+++ GraphlanInput/Guide_Add_Ring_Abundance_EMP.py
@@ -7,11 +7,14 @@ import math
 Abunin = open(sys.argv[1],'r')
 fwrite = open(sys.argv[2],'w')
 log = 0
-
+checkHeight = 1
 for line in Abunin:
     tempCol = line.strip().split('\t')
     fwrite.write(tempCol[0]+'\t'+"ring_width"+'\t'+"9"+'\t'+"1"+'\n')
     height = tempCol[2]
+    if (checkHeight == 1):
+        if(float(height) > 32767):
+            height = "32767"
     if(log == 1):
         x = math.log(float(height),10)
         height = str(x)
