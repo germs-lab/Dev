3a89cc710a1a8df5f4c47171c2a91ea014b963b8
Jin Choi <jinchoi721@gmail.com>
2016-02-25 16:38:36 -0600

    update

--- GraphlanInput/rdp_to_guide.py
+++ GraphlanInput/rdp_to_guide.py
@@ -6,7 +6,16 @@ import read_rdp_taxonomy
 filename = sys.argv[1]
 
 tax = read_rdp_taxonomy.get_tax(filename)
-
+dict = {}
 for item in tax:
-    print item[1]+'.'+item[2]+'.'+item[3]+'.'+item[4]+'.'+item[5]+'.'+item[6]
+    guide = item[1]+'.'+item[2]+'.'+item[3]+'.'+item[4]+'.'+item[5]+'.'+item[6]
+    if (dict.has_key(guide)):
+        temp = dict[guide]
+        newtemp = temp+','+item[0]
+        dict[guide] = newtemp
+    else:
+        dict[guide] = item[0]
 
+for x in dict.keys():
+    print x
+#print len(dict)
