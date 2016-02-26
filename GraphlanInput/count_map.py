b0e84b645543aaa6c007b43c31341a51e1411043
Jin Choi <jinchoi721@gmail.com>
2016-02-26 16:18:18 -0600

    update

--- GraphlanInput/count_map.py
+++ GraphlanInput/count_map.py
@@ -4,6 +4,8 @@ import sys
 
 sumread = open(sys.argv[1],'r')
 mapread = open(sys.argv[2],'r')
+
+#read summaryfile
 dict = {}
 line = sumread.next()
 while (line.strip() != 'Counts/sample detail:'):
@@ -11,5 +13,12 @@ while (line.strip() != 'Counts/sample detail:'):
 line = sumread.next()
 for line in sumread:
     splt = line.strip().split(': ')
-    print splt
-    
+    dict[splt[0]]=splt[1]
+sumread.close()
+#read map
+for line in mapread:
+    splt = line.strip().split('\t')
+    ids = splt[1].split(',')
+    for x in ids:
+        print dict[x]
+
