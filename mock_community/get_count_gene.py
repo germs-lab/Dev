f197efcece1590ed69257ac7411f7472f0154c65
Jin Choi <jinchoi721@gmail.com>
2016-05-17 17:53:39 -0500

    update

---
+++ mock_community/get_count_gene.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# this script get count for each ARG proteiin seq
+# usage: python get_count_gene.py list mock_protein_with_prodcut.fa map
+
+import sys
+lis = {}
+for line in open(sys.argv[1],'r'):
+    lis[line.strip()] = line.strip()
+
+for line in open(sys.argv[2],'r'):
+    if (line[:1] == ">"):
+        for keys in lis.keys():
+            if(keys in line):
+                print line,
+    else:
+        print line,
