9c2c740fa2b93a5fb9af0c6ea3852ff465826add
Jin Choi <jinchoi721@gmail.com>
2016-05-17 18:28:29 -0500

    update

--- mock_community/args_each_genome.py
+++ mock_community/args_each_genome.py
@@ -7,6 +7,7 @@ map = {}
 mapfile =open(sys.argv[1],'r')
 for line in mapfile:
     spl = line.rstrip().split('\t')
+    #print spl
     #print spl[1][1:]
     map[spl[1]] = spl[0]
 #print len(map)
@@ -15,22 +16,27 @@ for f in sys.argv[2:]:
     for line in open(f,'r'):
         if(line[:1]=="#"):
             continue
-        spl = line.rstrip().split(" ")
-        arg = f.split('.')[0].split('/')[1]
-        #print spl
-        if(genome.has_key(map[spl[0]])):
-            temp = genome[map[spl[0]]]
-            if(temp.has_key(arg)):
-                tetemp = temp[arg]
+        spl = line.rstrip().split(" - ")
+        query = spl[1].strip()
+        hit = spl[0].strip()
+        #print query, hit
+        #arg = f.split('.')[0].split('/')[1]
+        gen = map[query]
+        #print gen
+        if(genome.has_key(gen)):
+            temp = genome[gen]
+            if(temp.has_key(hit)):
+                tetemp = temp[hit]
                 tetemp += 1
-                temp[arg] = tetemp
+                temp[hit] = tetemp
+                genome[gen] = temp
             else:
-                temp[arg] = 1
-            
+                temp[hit] = 1
+                genome[gen] = temp
         else:
             temp = {}
-            temp[arg] = 1
-            genome[map[spl[0]]]=temp
+            temp[hit] = 1
+            genome[gen]=temp
 
 for item in genome.items():
     for res in item[1].items():
--- mock_community/get_count_gene.py
+++ mock_community/get_count_gene.py
@@ -6,11 +6,37 @@ import sys
 lis = {}
 for line in open(sys.argv[1],'r'):
     lis[line.strip()] = line.strip()
-
+#print lis
+map = {}
+for line in open(sys.argv[3],'r'):
+    spl = line.rstrip().split('\t')
+    map[spl[1]] = spl[0]
+genome = {}
 for line in open(sys.argv[2],'r'):
     if (line[:1] == ">"):
         for keys in lis.keys():
-            if(keys in line):
-                print line,
-    #else:
-    #    print line,
+            if(" "+keys.upper()+" " in line.upper()):
+                locus = line.split(' ')[0][1:]
+                gen = map[locus]
+                arg = keys
+                result = [map[locus],line.strip(),"::",keys]
+                #print '\t'.join(result)
+                if (genome.has_key(gen)):
+                    temp = genome[gen]
+                    if (temp.has_key(arg)):
+                        tetemp = temp[arg]
+                        tetemp += 1
+                        temp[arg] = tetemp
+                        genome[gen] = temp
+                    else:
+                        temp[arg] = 1
+                        genome[gen] = temp
+                else:
+                    temp = {}
+                    temp[arg] = 1
+                    genome[gen] = temp
+
+
+for item in genome.items():
+    for res in item[1].items():
+        print '\t'.join([item[0],res[0],str(res[1])])
